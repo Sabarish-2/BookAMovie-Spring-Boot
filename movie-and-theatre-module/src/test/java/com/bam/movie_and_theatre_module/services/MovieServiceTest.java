900854d164f9deab43a3f91cca41130063be889d
Sabarish-2 <sabarish.iyer3@gmail.com>
2025-12-25 18:44:04 +0530

    Swagger API Doc enabled in yml n conf, DTO: Validation, err.props n mapper Logging

---
+++ movie-and-theatre-module/src/test/java/com/bam/movie_and_theatre_module/services/MovieServiceTest.java
@@ -0,0 +1,9 @@
+package com.bam.movie_and_theatre_module.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovieServiceTest {
+    void test() {
+        assertNotNull("Abc");
+    }
+}
