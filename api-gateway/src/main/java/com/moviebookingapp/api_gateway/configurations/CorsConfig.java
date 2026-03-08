49b4f2b6a295e39a0549e9f5e16c5a7cb58cdaab
sabarish-2 <sabarishh03@gmail.com>
2026-03-08 10:53:06 +0530

    Add cors for netlify url

--- api-gateway/src/main/java/com/moviebookingapp/api_gateway/configurations/CorsConfig.java
+++ api-gateway/src/main/java/com/moviebookingapp/api_gateway/configurations/CorsConfig.java
@@ -1,12 +1,12 @@
 package com.moviebookingapp.api_gateway.configurations;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://bookmyshow-sabarish.netlify.app/"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
