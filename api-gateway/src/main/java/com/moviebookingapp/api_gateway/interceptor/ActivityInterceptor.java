3b4157f04287c7e2e15feee6c065871abdb96097
Sabarish Iyer <118958969+Sabarish-2@users.noreply.github.com>
2026-03-14 08:54:36 +0530

    Update ActivityInterceptor.java rem enab sched

--- api-gateway/src/main/java/com/moviebookingapp/api_gateway/interceptor/ActivityInterceptor.java
+++ api-gateway/src/main/java/com/moviebookingapp/api_gateway/interceptor/ActivityInterceptor.java
@@ -2,14 +2,13 @@ package com.moviebookingapp.api_gateway.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.moviebookingapp.api_gateway.scheduler.HealthScheduler;
 
 @Component
-@EnableScheduling
 public class ActivityInterceptor implements HandlerInterceptor {
 
 
