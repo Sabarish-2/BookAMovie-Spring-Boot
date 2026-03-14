b7cbb4509fe03237bff16fd081217c67340d52d8
Sabarish Iyer <118958969+Sabarish-2@users.noreply.github.com>
2026-03-14 09:09:44 +0530

    Update HealthScheduler.java update to add log rem enab sched add api link

--- api-gateway/src/main/java/com/moviebookingapp/api_gateway/scheduler/HealthScheduler.java
+++ api-gateway/src/main/java/com/moviebookingapp/api_gateway/scheduler/HealthScheduler.java
@@ -1,6 +1,5 @@
 package com.moviebookingapp.api_gateway.scheduler;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,16 +8,18 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Component
-@EnableScheduling
 public class HealthScheduler {
 
-    public static volatile long lastActivity = System.currentTimeMillis();
+    public volatile long lastActivity = System.currentTimeMillis();
+
+    private final RestTemplate restTemplate = new RestTemplate();
 
     private final List<String> services = List.of(
         "https://bookamovie-spring-boot-eureka-server-fe3s.onrender.com",
         "https://bookamovie-spring-boot-user-service.onrender.com",
         "https://bookamovie-spring-boot-tickets-service.onrender.com",
-        "https://bookamovie-spring-boot-movie-service-5c8d.onrender.com"
+        "https://bookamovie-spring-boot-movie-service-5c8d.onrender.com",
+"https://bookamovie-spring-boot-api-gateway-2psv.onrender.com"
     );
 
     @Scheduled(initialDelay = 0, fixedRate = 300000)
@@ -26,12 +27,16 @@ public class HealthScheduler {
         long now = System.currentTimeMillis();
         long diff = now - lastActivity;
 
+        System.out.println("Schedulre Start: \nDIFF - - " + diff);
         if (diff < 1800000) {
             for (String url: services) {
                 CompletableFuture.runAsync(() -> {
                     try {
-                        new RestTemplate().getForObject(url, String.class);
-                    } catch (Exception ignored) {}
+                        restTemplate.getForObject(url, String.class);
+                        System.out.println("Sucess? for url - " + url);
+                    } catch (Exception ex) {
+                        System.out.println("Error UrL - " + url + " Err is - " +  ex.getMessage());
+}
                 });
             }
         }
